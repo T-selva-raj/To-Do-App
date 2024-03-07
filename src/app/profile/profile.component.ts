99df38352ce27206703dcdf45edfafc658b48dd3
selva-rajT <selvaraj@centizen.com>
2024-03-07 16:00:55 +0530

    profile page changes

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  ngOnInit(): void {
+    let init = 0, target = 1001;
+
+    const updateCounter = () => {
+      if (init < target) {
+        setTimeout(() => {
+          init += 1;
+          counter.innerHTML = init.toString();
+
+          if (init > 100) {
+            counter.innerHTML = "100+";
+          }
+          if (init <= 100) {
+            updateCounter();
+          }
+        }, 15);
+      }
+    };
+
+    let counter = document.getElementById('counter') as HTMLElement;
+    counter.innerHTML = init.toString();
+    updateCounter();
+  }
+
 
 }
