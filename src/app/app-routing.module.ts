3420bfeced43870ebc8f8d5e0d80b66a5b5a64d7
selva-rajT <selvaraj@centizen.com>
2024-05-14 18:53:58 +0530

    routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { DashBoardDetailsComponent } from './dash-board-details/dash-board-details.component';
 import { ChartComponent } from './chart/chart.component';
 import { AddTaskComponent } from './add-task/add-task.component';
 import { AllTasksComponent } from './all-tasks/all-tasks.component';
 import { ProfileComponent } from './profile/profile.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { ReportComponent } from './report/report.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { DashBoardDetailsComponent } from './dash-board-details/dash-board-details.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginPageComponent },
-  { path: 'dashboard', component: DashBoardDetailsComponent },
-  { path: 'profile', component: ProfileComponent },
-  { path: 'add', component: AddTaskComponent },
-  { path: 'all', component: AllTasksComponent },
-  { path: 'report', component: ReportComponent },
-  { path: 'contactus', component: ContactUsComponent },
-
+  { path: 'register', component: SignupPageComponent },
+  {
+    path: 'app', component: DashBoardComponent, children: [
+      { path: 'dashboard', component: DashBoardDetailsComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'add', component: AddTaskComponent },
+      { path: 'all', component: AllTasksComponent },
+      { path: 'report', component: ReportComponent },
+      { path: 'contactus', component: ContactUsComponent },
+    ]
+  }
 ];
 
 @NgModule({
---
+++ src/enviorment.ts
@@ -0,0 +1,15 @@
+
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyCS2-JecXYtqfRmOmvIL-q-kFh2VD86kyQ",
+        authDomain: "test-app-d051b.firebaseapp.com",
+        databaseURL: "https://test-app-d051b-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "test-app-d051b",
+        storageBucket: "test-app-d051b.appspot.com",
+        messagingSenderId: "884943927455",
+        appId: "1:884943927455:web:3ffddbe47f65c600768aaa",
+        measurementId: "G-NVKDSPNXPK"
+    }
+}
+
