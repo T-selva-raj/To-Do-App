82bdf27c121422a2baa4c6464cc54ff1addcb837
selva-rajT <selvaraj@centizen.com>
2024-06-08 16:48:12 +0530

    close side nav while smaller device

--- src/app/dash-board/dash-board.component.ts
+++ src/app/dash-board/dash-board.component.ts
@@ -1,53 +1,66 @@
 import { AfterViewInit, Component, OnDestroy, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { Router } from '@angular/router';
 import { DialogService } from '../shared/services/dialog.service';
 import { MatDialogRef } from '@angular/material/dialog';
 import { DialogComponent } from '../shared/components/dialog/dialog.component';
 import { Subscription } from 'rxjs';
+
 @Component({
   selector: 'app-dash-board',
   templateUrl: './dash-board.component.html',
   styleUrls: ['./dash-board.component.css']
 })
 export class DashBoardComponent implements AfterViewInit, OnDestroy {
+
   @ViewChild(MatSidenav)
   sidenav!: MatSidenav;
   title = "sample"
   dialogRef!: MatDialogRef<DialogComponent>;
   isLoader: boolean = false;
   subscriptionObj = new Subscription();
-  constructor(private observer: BreakpointObserver,
-    private route: Router,
-    private dialogService: DialogService) { }
+  isSmallScreen: boolean = false;
 
+  constructor(
+    private observer: BreakpointObserver,
+    private route: Router,
+    private dialogService: DialogService
+  ) { }
 
   ngAfterViewInit() {
-    this.observer.observe(["(max-width: 800px)"]).subscribe((res) => {
-      if (res.matches) {
+    this.subscriptionObj.add(this.observer.observe(["(max-width: 800px)"]).subscribe((res: BreakpointState) => {
+      this.isSmallScreen = res.matches;
+      if (this.isSmallScreen) {
         this.sidenav.mode = "over";
         this.sidenav.close();
       } else {
         this.sidenav.mode = "side";
         this.sidenav.open();
       }
-    });
+    }));
   }
 
   logout() {
     this.dialogRef = this.dialogService.openDialog({
       title: "Alert",
-      message: "Do you want to  log out ?"
+      message: "Do you want to log out?"
     });
     this.subscriptionObj.add(this.dialogRef.afterClosed().subscribe((res: any) => {
       if (res) {
         this.isLoader = true;
         localStorage.clear();
-        this.route.navigate(['login'])
+        this.route.navigate(['login']);
       }
     }));
   }
+
+  closeNav() {
+    if (this.isSmallScreen) {
+      this.sidenav.close();
+    }
+  }
+
   ngOnDestroy() {
     this.subscriptionObj.unsubscribe();
   }
