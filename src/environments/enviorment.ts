247c010811b92f200d298074d21f9a05a47cf441
selva-rajT <selvaraj@centizen.com>
2024-06-10 18:29:26 +0530

    no message

--- src/environments/enviorment.ts
+++ src/environments/enviorment.ts
@@ -1,25 +1,9 @@
-
-
-interface env {
-    production: boolean,
-    firebase: fire_base
-}
-interface fire_base {
-    apiKey: string
-    authDomain: string,
-    databaseURL: string,
-    projectId: string,
-    storageBucket: string,
-    messagingSenderId: string,
-    appId: string,
-    measurementId: string
-}
-export const environment: env = {
+export const environment = {
     production: false,
     firebase: {
         apiKey: "AIzaSyCS2-JecXYtqfRmOmvIL-q-kFh2VD86kyQ",
         authDomain: "test-app-d051b.firebaseapp.com",
-        databaseURL: "https://test-app-d051b-default-rtdb.asia-southeast1.firebasedatabase.app",
+        // databaseURL: "https://test-app-d051b-default-rtdb.asia-southeast1.firebasedatabase.app",
         projectId: "test-app-d051b",
         storageBucket: "test-app-d051b.appspot.com",
         messagingSenderId: "884943927455",
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,19 +1,4 @@
-interface env {
-    production: boolean,
-    firebase: fire_base
-}
-interface fire_base {
-    apiKey: string
-    authDomain: string,
-    databaseURL: string,
-    projectId: string,
-    storageBucket: string,
-    messagingSenderId: string,
-    appId: string,
-    measurementId: string
-}
-
-export const enviorment: env = {
+export const environment = {
     production: true,
     firebase: {
         apiKey: "AIzaSyDPYvUVf7mF4L2anS6pRx_-4C6AG3DxvwY",
@@ -22,7 +7,6 @@ export const enviorment: env = {
         storageBucket: "todo-dev-f5aaa.appspot.com",
         messagingSenderId: "910768977884",
         appId: "1:910768977884:web:b0b75212d48f97ac4c4119",
-        measurementId: "G-YNP96BDJ0V",
-        databaseURL: ""
+        measurementId: "G-YNP96BDJ0V"
     }
 }
