8474d0a41196a04ec79dc3af63dcb5f52f745ae2
selva-rajT <selvaraj@centizen.com>
2024-06-11 12:33:24 +0530

    Revert "server initial commit"
    This reverts commit ccdf7b7f5948104c703551a1c187e9ed914df12a.

--- src/app/shared/constants/messages.ts
+++ src/app/shared/constants/messages.ts
@@ -1,4 +1,3 @@
 export const MESSAGES = {
-    LOGOUT_CONFIRMATION: "Do you really want to log out ?",
-    UNSAVED_CHANGES: "You have some unsaved changes, Do you really want to exit ?"
+    LOGOUT_CONFIRMATION: "Do you really want to log out ?"
 }
