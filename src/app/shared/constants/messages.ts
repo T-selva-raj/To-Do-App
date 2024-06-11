ccdf7b7f5948104c703551a1c187e9ed914df12a
selva-rajT <selvaraj@centizen.com>
2024-06-11 12:32:24 +0530

    server initial commit

--- src/app/shared/constants/messages.ts
+++ src/app/shared/constants/messages.ts
@@ -1,3 +1,4 @@
 export const MESSAGES = {
-    LOGOUT_CONFIRMATION: "Do you really want to log out ?"
+    LOGOUT_CONFIRMATION: "Do you really want to log out ?",
+    UNSAVED_CHANGES: "You have some unsaved changes, Do you really want to exit ?"
 }
