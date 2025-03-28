bd2b6ed366cefdffe17e9689c05a6f7fce8ee761
T-selva-raj <selvarajpersonal24@gmail.com>
2025-03-28 10:28:34 +0530

    no message

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,5 @@
 export const environment = {
     production: true,
-    firebase: {
-        apiKey: "AIzaSyDPYvUVf7mF4L2anS6pRx_-4C6AG3DxvwY",
-        authDomain: "todo-dev-f5aaa.firebaseapp.com",
-        projectId: "todo-dev-f5aaa",
-        storageBucket: "todo-dev-f5aaa.appspot.com",
-        messagingSenderId: "910768977884",
-        appId: "1:910768977884:web:b0b75212d48f97ac4c4119",
-        measurementId: "G-YNP96BDJ0V"
-    }
+    server: 'https://todo-server-ikh6.onrender.com/',
+    secret_key: 'mykey1234'
 };
