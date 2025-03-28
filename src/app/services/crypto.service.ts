4ee120c21f6c346d0f3df9919728ac65c24eaa2b
T-selva-raj <selvarajpersonal24@gmail.com>
2025-03-28 12:08:44 +0530

    no message

--- src/app/services/crypto.service.ts
+++ src/app/services/crypto.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
-import { environment } from '../../environments/enviorment';
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
